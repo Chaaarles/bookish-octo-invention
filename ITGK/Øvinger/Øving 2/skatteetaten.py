5b596af7d49d712bb761f50717f8c8c7763f28a5
Petter Charles Redfern <pettercr@stud.ntnu.no>
2018-09-14 16:15:19 +0200

    Finished skatteetaten.py

--- ITGK/Øvinger/Øving 2/skatteetaten.py
+++ ITGK/Øvinger/Øving 2/skatteetaten.py
@@ -33,9 +33,8 @@ def primær():
 
 def fritid():
     print("INFO")  # Info
-    print("Dette programmet besvarer om din utleie en annen type bolig,")
-    print("her fritidsbolig, er skattepliktig.")
-    print("Vi trenger vi først å vite om fritidsboligen(e) primært brukes til utleie eller fritid.")
+    print("Dette programmet besvarer om din utleie av fritidsbolig er skattepliktig.")
+    print("Først trenger vi å vite om fritidsboligen(e) primært brukes til utleie eller fritid.")
     print("Deretter trenger vi å vite hvor mange fritidsboliger du leier ut.")
     print("Til slutt trenger vi å vite hvor store utleieinntekter du har pr. fritidsbolig.")
     linje()
@@ -48,10 +47,54 @@ def fritid():
 
     print("SKATTEBEREGNING")  # Skatteberegning
     if formål == "utleie" or inntekt > 10000:
-        if formål == "utleie":
-            rabatt = 10000
         print("Inntekten er skattepliktig")
-        if formål == :
-            pass
+        if formål == "fritid":
+            inntekt -= 10000
+            print("Overskytende beløp pr. fritidsbolig er", inntekt)
+            inntekt *= 0.85
+            print("Skattepliktig inntekt pr fritidsbolig er", inntekt)
+        else:
+            print("Skattepliktig inntekt pr fritidsbolig er", inntekt)
+        print("Totalt skatteplikltig beløp er", inntekt * antall)
         print
+    else:
+        print("Inntekten er ikke skattepliktig")
         
+
+def sekundær():
+    print("INFO")  # Info
+    print("Dette programmet besvarer om din utleie av sekundærbolig er skattepliktig.")
+    print("Først trenger vi å vite hvor mange sekundærboliger du leier ut.")
+    print("Deretter trenger vi å vite hvor store utleieinntekter du har pr. sekundærbolig.")
+    linje()
+
+    print("DATAINNHENTING")  # Datainnhenting
+    antall = int(input("Skriv inn antallet sekundærboliger du leier ut: "))
+    inntekt = int(input("Skriv inn utleieinntekten pr. sekundærbolig: "))
+    linje()
+
+    print("SKATTEBEREGNING")  # Skatteberegning
+    print("Inntekten er skattepliktig") 
+    print("Skattepliktig inntekt pr sekundærbolig er", inntekt)
+    print("Totalt skatteplikltig beløp er", inntekt * antall)
+    print
+        
+def valg():
+    print("INFO")  # Info
+    print("Velkommen til denne skattekalkulatoren!")
+    print("Her kan du finne ut om dine leieinntekter på diverse boliger er skattepliktige.")
+    print("Først trenger vi å vite hvilken type bolig du har leid ut.")
+    linje()
+
+    print("DATAINNHENTING")  # Datainnhenting
+    valg = input("Hvilke boligtype vil du kalkulere? (primær/sekundær/fritid): ").lower()
+    linje()
+
+    if valg == "primær":
+        primær()
+    elif valg == "sekundær":
+        sekundær()
+    else:
+        fritid()
+
+valg()
